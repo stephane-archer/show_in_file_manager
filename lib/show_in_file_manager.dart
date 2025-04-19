df768f6d1c8e061972ae5029560a6f424869d1cb
Archer Stéphane <2981437+stephane-archer@users.noreply.github.com>
2025-04-19 18:17:51 +0200

    add macOS code

--- lib/show_in_file_manager.dart
+++ lib/show_in_file_manager.dart
@@ -1,5 +1,28 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:io';
+
+import 'package:open_file_macos/open_file_macos.dart';
+import 'package:path/path.dart' as path_lib;
+
+Future<void> showInFileManager(String path) async {
+  final fileSystemEntityType = FileSystemEntity.typeSync(path);
+  if (fileSystemEntityType == FileSystemEntityType.notFound) {
+    throw PathNotFoundException(path, OSError());
+  }
+  if (Platform.isMacOS) {
+    _showInFinder(path);
+    return;
+  }
+  if (Platform.isWindows) {
+    // TODO: add Windows support
+    return;
+  }
+  throw UnsupportedError("Unsupported Platform");
+}
+
+Future<void> _showInFinder(String path) async {
+  if (!path_lib.isAbsolute(path)) {
+    path = path_lib.absolute(path);
+  }
+  final openFileMacosPlugin = OpenFileMacos();
+  await openFileMacosPlugin.open(path, viewInFinder: true);
 }
